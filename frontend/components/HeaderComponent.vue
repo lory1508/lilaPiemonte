66525b869fb7d4750cefeb23d67d8096e200b4b9
Lorenzo <lorenzo.galassi91@gmail.com>
2025-03-25 11:39:30 +0000

    feat: home WIP

--- frontend/components/HeaderComponent.vue
+++ frontend/components/HeaderComponent.vue
@@ -1,8 +1,38 @@
 <template>
-  <div class="h-4">Ciao</div>
+  <div class="h-16 flex flex-row justify-between bg-black bg-opacity-15 fixed w-full p-2 text-white">
+    <div class="flex flex-row gap-6 items-center montserrat">
+      <div v-for="(ll, index) in leftLinks" :key="`ll_${index}`" :class="ll.active ? 'underline font-semibold' : ''">
+        <NuxtLink :to="ll.href">{{ ll.title }}</NuxtLink>
+      </div>
+    </div>
+    <img :src="logo" alt="" />
+    <div class="flex flex-row gap-6 items-center montserrat">
+      <div v-for="(rl, index) in rightLinks" :key="`rl_${index}`" :class="rl.active ? 'underline font-semibold' : ''">
+        <NuxtLink
+          v-if="index == rightLinks.length - 1"
+          :to="rl.href"
+          class="bg-white flex flex-row items-center gap-3 border-2 hover:scale-105 transition-all duration-300 sharp-shadow shadow-red-600 border-red-600 px-2 py-1 text-red-600 font-semibold"
+        >
+          <Icon icon="noto-v1:growing-heart" />
+          {{ rl.title }}
+        </NuxtLink>
+        <NuxtLink v-else :to="rl.href">{{ rl.title }}</NuxtLink>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-  const t = ''
-  console.log(t)
+  import data from '~/utils/data.json'
+  import { Icon } from '@iconify/vue'
+
+  const leftLinks = ref([])
+  const rightLinks = ref([])
+  const logo = ref('')
+
+  onMounted(() => {
+    leftLinks.value = data.menu.linksLeft
+    rightLinks.value = data.menu.linksRight
+    logo.value = data.generalSettings.logo
+  })
 </script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,46 @@
 <template>
-  <div></div>
+  <div class="flex flex-col">
+    <div
+      v-if="!loading"
+      class="w-screen h-screen"
+      :style="`background-position: 50% 98%; background-size: auto; background-image: url('${hero.imgBackground}'); background-attachment: fixed;`"
+    >
+      test
+    </div>
+    <div v-else>LOADING!</div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+  import data from '~/utils/data.json'
 
-<style></style>
+  const loading = ref(false)
+  const hero = ref()
+  const nextEvent = ref()
+  const cards = ref()
+  const services = ref()
+  const socials = ref()
+  const video = ref()
+
+  const timeout = (milliseconds) => {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds))
+  }
+  onMounted(async () => {
+    try {
+      loading.value = true
+      await timeout(20000)
+      hero.value = data.home.hero
+      nextEvent.value = data.home.nextEvent
+      cards.value = data.home.cards
+      services.value = data.home.services
+      socials.value = data.home.socials
+      video.value = data.home.video
+
+      console.log(hero.value)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      loading.value = false
+    }
+  })
+</script>
