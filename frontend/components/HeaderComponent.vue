db4775b3e5ed1fded385674219a4f365b0149e13
Lorenzo <lorenzo.galassi91@gmail.com>
2025-03-25 09:21:20 +0000

    feat: tailwind init

---
+++ frontend/components/HeaderComponent.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="h-4">Ciao</div>
+</template>
+
+<script setup>
+  const t = ''
+  console.log(t)
+</script>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/eslint']
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: ['@nuxt/fonts', '@nuxt/eslint'],
 })
